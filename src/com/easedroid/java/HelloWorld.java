bc9d719c027ad8c28b5e621e96e5664150823a45
jingbin4679 <752368300@qq.com>
2017-06-24 16:09:40 +0800

    first commit

---
+++ src/com/easedroid/java/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.easedroid.java;
+
+/**
+ * Created by jingbin on 2017/6/24.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello World !");
+    }
+}
